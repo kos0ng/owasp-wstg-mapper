487796cbef93ab23fbad0582ed627329d1e57318
kos0ng <kosongisnotzero@gmail.com>
2023-05-14 22:44:13 +0700

    feat: optimize mapper feature, check redundant url, files url

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import xml.etree.ElementTree as ET
 import argparse
 import base64
+import re
+from uuid import UUID
 from mapper import mapper
+
+
+def isValidUUID(uuid_to_test, version=4):
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
+
+def formatURL(url):
+	tmp = url.split("/")
+	regexURL = r"[\?]?([\w]+)=([\w-]+)"
+	partURL = []
+	for i in tmp:
+		result = re.findall(regexURL, i)
+		if(result != []):
+			partURL.append(result)
+		else:
+			partURL.append(i)
+	formatted = []
+	for i in partURL:
+		if(isinstance(i, list)):
+			parameter = []
+			for j in i:
+				parameter.append(j[0] + "=<value>")
+			formatted.append("?" + "&".join(parameter))
+		elif(i.isdigit()):
+			formatted.append("<value>")
+		elif(isValidUUID(i)):
+			formatted.append("<value>")
+		else:
+			formatted.append(i)
+	return "/".join(formatted)
 
 def normalizeURL(url):
 	
@@ -10,7 +45,8 @@ def normalizeURL(url):
 	endpoint = tmp[1]
 	endpoint = endpoint.replace("//","/")
 	
-	return f"{protocol}://{endpoint}"
+	return f"{protocol}://{formatURL(endpoint)}"
+
 
 def parseXML(xmlFile):
   
@@ -59,6 +95,7 @@ def parseHTTP(data):
 
 if __name__ == "__main__":
 	
+	listURL = []
 	parser = argparse.ArgumentParser(description='Mapper description')
 	parser.add_argument("-f", "--filename", type=str)
 	parser.add_argument("-o", "--output", type=str)
--- mapper.py
+++ mapper.py
@@ -2,29 +2,53 @@ import json
 import re
 from export import *
 
-def check(key, data, jsonData):
-	for i in jsonData:
-		tmp = jsonData[i]['test']
-		for j in tmp:
-			for k in tmp[j]['regex']:
-				x = re.search(k, data)
+def removeFilesURL(blacklist, keys):
+	sanitized = []
+	for i in keys:
+		check = True
+		for j in blacklist:
+			if(i.endswith(j)):
+				check = False
+				break
+		if(check):
+			sanitized.append(i)
+	return sanitized
+
+def assign(testData, data):
+	keyData = list(data.keys())
+	if(testData['files'] == 0):
+		blacklist = [".png", ".jpg", ".jpeg", ".json", ".css", ".js", ".ico"]
+		keyData = removeFilesURL(blacklist, keyData)
+	testCase = testData['test']
+	request = testCase['request']
+	response = testCase['response']
+	for i in request['regex']:
+		for j in keyData:
+			x = re.search(i, data[j]['request']['header'])
+			if(x is None):
+				x = re.search(i, data[j]['request']['body'])
 				if(x is not None):
-					jsonData[i]['target'].append(key)
-					break
-			for k in tmp[j]['basic']:
-				if(k in data):
-					jsonData[i]['target'].append(key)
-					break
-	return jsonData
+					testData['target'].append(j)
+					keyData.remove(j)
+			else:
+				testData['target'].append(j)
+				keyData.remove(j)
+	for i in response['regex']:
+		for j in keyData:
+			x = re.search(i, data[j]['response']['header'])
+			if(x is None):
+				x = re.search(i, data[j]['response']['body'])
+				if(x is not None):
+					testData['target'].append(j)
+					keyData.remove(j)
+			else:
+				testData['target'].append(j)
+				keyData.remove(j)
+	return testData
 
 def mapper(data, output = None, level = None):
 	f = open("data/wstg.json","r").read()
 	jsonData = json.loads(f)
-	
-	for i in data:
-		jsonData = check(i, data[i]['request']['header'],jsonData)
-		jsonData = check(i, data[i]['request']['body'],jsonData)
-		jsonData = check(i, data[i]['response']['header'],jsonData)
-		jsonData = check(i, data[i]['response']['body'],jsonData)
-
+	for i in jsonData:
+		jsonData[i] = assign(jsonData[i], data)
 	export(jsonData, output, level)
